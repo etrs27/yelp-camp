20d8d11efb720141ef1613ec9b9b14f4c38425ff
Theodore Stansbury <tstansbu.27@gmail.com>
2022-05-23 12:42:18 -0400

    Edited styling for index.

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-const Review = require('../models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
